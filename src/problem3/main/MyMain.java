2418566df5e7afb315bd06e1b45e0ba5abe3e523
gaursuniverse <vishal.gaur_cs18@gla.ac.in>
2020-03-28 15:25:44 +0530

    final commit

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("vishal",1);
+        obj.insert("gaur",4);
+        obj.insert("vishu",3);
+        obj.insert("vishv",2);
+        obj.insert("gaursuniverse",5);
+        obj.display();
+    }
 }
